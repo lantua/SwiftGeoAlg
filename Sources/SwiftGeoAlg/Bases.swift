c20dedf8aed9dc5807c7b740f7a56b06cf473ec0
Lantua <supergunman.na@gmail.com>
2020-05-28 16:21:04 -0400

    Removed Basis from `Vector` signature.

--- Sources/SwiftGeoAlg/Bases.swift
+++ Sources/SwiftGeoAlg/Bases.swift
@@ -21,7 +21,7 @@ public enum EndChain: BasisChain {
     public typealias MultiVector = Scalar
 }
 public enum Chained<Current: Basis, Next: BasisChain>: BasisChain {
-    public typealias MultiVector = Vector<Current, Next.MultiVector, Next.MultiVector>
+    public typealias MultiVector = Vector<Next.MultiVector, Next.MultiVector>
 }
 
 // MARK: Parity
@@ -34,8 +34,8 @@ extension EndChain: ParityBasisChain {
     public typealias Even = Scalar
 }
 extension Chained: ParityBasisChain where Next: ParityBasisChain {
-    public typealias Odd = Vector<Current, Next.Even, Next.Odd>
-    public typealias Even = Vector<Current, Next.Odd, Next.Even>
+    public typealias Odd = Vector<Next.Even, Next.Odd>
+    public typealias Even = Vector<Next.Odd, Next.Even>
 }
 
 // MARK: Grading
@@ -55,20 +55,23 @@ extension EndChain: GradedBasisChain {
     public typealias Grade0 = Scalar
 }
 extension Chained: GradedBasisChain where Next: GradedBasisChain {
-    public typealias Grade0 = Vector<Current, Empty, Next.Grade0>
-    public typealias Grade1 = Vector<Current, Next.Grade0, Next.Grade1>
-    public typealias Grade2 = Vector<Current, Next.Grade1, Next.Grade2>
-    public typealias Grade3 = Vector<Current, Next.Grade2, Next.Grade3>
-    public typealias Grade4 = Vector<Current, Next.Grade3, Next.Grade4>
-    public typealias Grade5 = Vector<Current, Next.Grade4, Next.Grade5>
-    public typealias Grade6 = Vector<Current, Next.Grade5, Next.Grade6>
-    public typealias Grade7 = Vector<Current, Next.Grade6, Next.Grade7>
-    public typealias Grade8 = Vector<Current, Next.Grade7, Next.Grade8>
+    public typealias Grade0 = Vector<Empty, Next.Grade0>
+    public typealias Grade1 = Vector<Next.Grade0, Next.Grade1>
+    public typealias Grade2 = Vector<Next.Grade1, Next.Grade2>
+    public typealias Grade3 = Vector<Next.Grade2, Next.Grade3>
+    public typealias Grade4 = Vector<Next.Grade3, Next.Grade4>
+    public typealias Grade5 = Vector<Next.Grade4, Next.Grade5>
+    public typealias Grade6 = Vector<Next.Grade5, Next.Grade6>
+    public typealias Grade7 = Vector<Next.Grade6, Next.Grade7>
+    public typealias Grade8 = Vector<Next.Grade7, Next.Grade8>
 }
 
 // MARK: Helpers
 
 public typealias Base1<B1: Basis> = Chained<B1, EndChain>
 public typealias Base2<B1: Basis, B2: Basis> = Chained<B1, Base1<B2>>
-public typealias Base3<B1: Basis, B2: Basis, B3: Basis> = Bases<B1, B2, B3, EndChain>
-public typealias Bases<B1: Basis, B2: Basis, B3: Basis, Tail: BasisChain> = Chained<B1, Chained<B2, Chained<B3, Tail>>>
+public typealias Base3<B1: Basis, B2: Basis, B3: Basis> = Chained<B1, Base2<B2, B3>>
+public typealias Base4<B1: Basis, B2: Basis, B3: Basis, B4: Basis> = Chained<B1, Base3<B2, B3, B4>>
+public typealias Base5<B1: Basis, B2: Basis, B3: Basis, B4: Basis, B5: Basis> = Chained<B1, Base4<B2, B3, B4, B5>>
+public typealias Bases<B1: Basis, B2: Basis, B3: Basis, B4: Basis, B5: Basis, Tail: BasisChain> =
+    Chained<B1, Chained<B2, Chained<B3, Chained<B4, Chained<B5, Tail>>>>>
--- Sources/SwiftGeoAlg/VectorStorage.swift
+++ Sources/SwiftGeoAlg/VectorStorage.swift
@@ -8,64 +8,51 @@
 import Foundation
 
 public protocol VectorStorage {
-    associatedtype Included: VectorStorage = Empty
-    associatedtype Excluded: VectorStorage = Empty
-    var included: Included { get set }
-    var excluded: Excluded { get set }
+    typealias ScalarValue = Double
+    associatedtype Included: VectorStorage = Never
+    associatedtype Excluded: VectorStorage = Never
 
     init()
-    mutating func add<Other: VectorStorage>(other: Other)
-    mutating func subtract<Other: VectorStorage>(other: Other)
+
+    static var isEmpty: Bool { get }
+    var scalar: ScalarValue { get }
+    var included: Included { get }
+    var excluded: Excluded { get }
 }
 
-public extension VectorStorage where Included == Empty, Excluded == Empty {
-    var included: Included { get { unreachable() } set { unreachable() } }
-    var excluded: Excluded { get { unreachable() } set { unreachable() } }
+public extension VectorStorage where Included == Never, Excluded == Never {
+    var included: Included { unreachable() }
+    var excluded: Excluded { unreachable() }
+}
+
+extension Never: VectorStorage {
+    public init() { unreachable() }
+    public static var isEmpty: Bool { unreachable() }
+    public var scalar: ScalarValue { unreachable() }
+}
+
+public struct Empty: VectorStorage {
+    public init() { }
+    public static var isEmpty: Bool { true }
+    public var scalar: ScalarValue { unreachable() }
 }
 
 public struct Scalar: VectorStorage {
-    var value: Double
+    public var value: Double
+    public init(value: Double) { self.value = value }
 
-    public init() { value = 0 }
-    public mutating func add<Other>(other: Other) where Other : VectorStorage {
-        if let other = other as? Self {
-            value += other.value
-        }
-    }
-    public mutating func subtract<Other>(other: Other) where Other : VectorStorage {
-        if let other = other as? Self {
-            value -= other.value
-        }
-    }
-}
-public struct Empty: VectorStorage {
-    public init() { }
-    public func add<Other>(other: Other) where Other : VectorStorage { /* Do nothing */ }
-    public func subtract<Other>(other: Other) where Other : VectorStorage { /* Do nothing */ }
+    public init() { value = .zero }
+    public static var isEmpty: Bool { false }
+    public var scalar: ScalarValue { value }
 }
 
-public struct Vector<Current: Basis, Included: VectorStorage, Excluded: VectorStorage>: VectorStorage {
+public struct Vector<Included: VectorStorage, Excluded: VectorStorage>: VectorStorage {
     public var included: Included, excluded: Excluded
 
     public init() {
         included = .init()
         excluded = .init()
     }
-
-    public mutating func add<Other>(other: Other) where Other : VectorStorage {
-        if Other.Included.self != Empty.self {
-            included.add(other: other.included)
-        }
-        if Other.Excluded.self != Empty.self {
-            excluded.add(other: other.excluded)
-        }
-    }
-    public mutating func subtract<Other>(other: Other) where Other : VectorStorage {
-        if Other.Included.self != Empty.self {
-            included.subtract(other: other.included)
-        }
-        if Other.Excluded.self != Empty.self {
-            excluded.subtract(other: other.excluded)
-        }
-    }
+    public static var isEmpty: Bool { Included.isEmpty && Excluded.isEmpty }
+    public var scalar: ScalarValue { unreachable() }
 }
