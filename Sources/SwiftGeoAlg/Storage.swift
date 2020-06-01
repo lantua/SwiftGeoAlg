4ab79ed6f273375c036b518422a22917ac4ec83b
Lantua <supergunman.na@gmail.com>
2020-06-01 11:14:38 -0400

    Separated some initialisers from `Storage` as they facilitate *silent discarding* of arguments.

--- Sources/SwiftGeoAlg/Storage.swift
+++ Sources/SwiftGeoAlg/Storage.swift
@@ -7,13 +7,11 @@
 
 public typealias ScalarValue = Double
 
-public protocol Storage: ExpressibleByFloatLiteral {
+public protocol Storage {
     associatedtype Included: Storage
     associatedtype Excluded: Storage
 
     init()
-    init(scalar: ScalarValue)
-    init(included: Included, excluded: Excluded)
 
     var included: Included { get set }
     var excluded: Excluded { get set }
@@ -21,16 +19,13 @@ public protocol Storage: ExpressibleByFloatLiteral {
 }
 
 #warning("Todo: Remove this")
-public extension Storage {
+public extension Mixed {
     @inlinable static func included(_ included: Included) -> Self { .init(included: included, excluded: .init()) }
     @inlinable static func excluded(_ excluded: Excluded) -> Self { .init(included: .init(), excluded: excluded) }
-    @inlinable init(floatLiteral value: Double.FloatLiteralType) { self.init(scalar: .init(floatLiteral: value)) }
 }
 
 public struct Empty: Storage {
     @inlinable public init() { }
-    @inlinable public init(scalar: ScalarValue) { }
-    @inlinable public init(included: Empty, excluded: Empty) { }
 
     public var included: Empty { get { unreachable() } set { } }
     public var excluded: Empty { get { unreachable() } set { } }
@@ -38,9 +33,7 @@ public struct Empty: Storage {
 }
 
 public struct Scalar: Storage {
-    @inlinable public init() { scalar = .zero }
-    @inlinable public init(scalar: ScalarValue) { self.scalar = scalar }
-    @inlinable public init(included: Empty, excluded: Scalar) { self = excluded }
+    @inlinable public init() { scalar = .init() }
 
     @inlinable public var included: Empty { get { .init() } set { } }
     @inlinable public var excluded: Self { get { self } set { self = newValue } }
@@ -49,7 +42,6 @@ public struct Scalar: Storage {
 
 public struct Mixed<Included: Storage, Excluded: Storage>: Storage {
     @inlinable public init() { excluded = .init() }
-    @inlinable public init(scalar: ScalarValue) { excluded = .init(scalar: scalar) }
     @inlinable public init(included: Included, excluded: Excluded) {
         self.included = included
         self.excluded = excluded
--- Sources/SwiftGeoAlg/Utilities.swift
+++ Sources/SwiftGeoAlg/Utilities.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 
+// MARK: Byte zeroable
+
 public protocol ByteZeroable: Storage { }
 public extension ByteZeroable {
     mutating func reset() { _ = withUnsafeMutableBytes(of: &self) { memset($0.baseAddress, 0, $0.count) } }
@@ -15,3 +17,12 @@ public extension ByteZeroable {
 extension Empty: ByteZeroable { }
 extension Scalar: ByteZeroable { }
 extension Mixed: ByteZeroable where Included: ByteZeroable, Excluded: ByteZeroable { }
+
+// MARK: ExpressibleByFloatLiteral
+
+extension Scalar: ExpressibleByFloatLiteral {
+    @inlinable public init(floatLiteral value: ScalarValue.FloatLiteralType) { scalar = .init(floatLiteral: value) }
+}
+extension Mixed where Excluded: ExpressibleByFloatLiteral {
+    @inlinable public init(floatLiteral value: Excluded.FloatLiteralType) { excluded = .init(floatLiteral: value) }
+}
